8afdca5d9d4932519b898def7ad5f3d5703437ea
Werner Schwarz <wegu87@gmail.com>
2011-12-05 17:18:22 +0100

    changed oper to a bot-hook

--- plugins/oper.js
+++ plugins/oper.js
@@ -1,27 +1,22 @@
-var Commands = require('../lib/commands')
+/*
+ *Automatically tries to op joining people given in a oper-list.
+ */
+module.exports = function(bot) {
+  bot._oper = new Array('slup', 'topaxi')
+  bot.on('join', function(user) {
+    var nick = user.split('!')[0]
 
-Commands.add('oper'
-  , 'If bot is OP, OPs joining people listed in the oper-list.'
-  , function(bot) {
-    bot._oper = new Array('example', 'admin')
-    bot.irc.on('join', function(user) {
-      var nick = user.split('!')[0]
+    if (~bot._oper.indexOf(nick)) {
+      say('Welcome back, master ' + nick)
+      op(nick)
+    } 
+  })
 
-      if (~bot._oper.indexOf(nick)) {
-	say('Welcome back, master ' + nick)
-	op(nick)
-      } 
-    })
-
-    function op(nick) {
-      bot.irc.write('MODE ' + bot.channel + ' +o ' + nick)
-    }
-
-    function say(text) {
-      bot.say(text)
-    }
+  function op(nick) {
+    bot.irc.write('MODE ' + bot.channel + ' +o ' + nick)
   }
- , function(from, to, nick) {
-   /* not used */
+
+  function say(text) {
+    bot.say(text)
   }
-)
+}
