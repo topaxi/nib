76eb1dc4454bcbeccdc0c0682c663106e8a2de21
Werner Schwarz <wegu87@gmail.com>
2011-12-11 04:21:32 +0100

    oper plugin now uses opers file for list of ops.

--- plugins/oper/oper.js
+++ plugins/oper/oper.js
@@ -1,14 +1,12 @@
 /*
- *Automatically tries to op joining people given in a oper-list.
+ *Automatically tries to op joining people listed in a oper-list.
  */
 module.exports = function(bot) {
-	  
-  bot._oper = ['slup', 'topaxi']
+  bot._oper = require('./opers.json')  
   bot.irc.on('join', function(user) {
     var nick = user.split('!')[0]
 
     if (~bot._oper.indexOf(nick)) {
-      bot.say('Welcome back master ' + nick)
       op(nick)
     } 
   })
