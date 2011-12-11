5ea4b86a15f242ab3ccf68c8e4493f189bccfc11
Damian Senn <damian.senn@gmail.com>
2011-12-11 15:25:18 +0100

    use function scoped variable for opers

--- plugins/oper/oper.js
+++ plugins/oper/oper.js
@@ -1,17 +1,16 @@
-/*
- *Automatically tries to op joining people listed in a oper-list.
+/**
+ * Automatically tries to op joining people listed in a oper-list.
  */
 module.exports = function(bot) {
-  bot._oper = require('./opers.json')  
+  var opers = require('./opers.json')
+
   bot.irc.on('join', function(user) {
     var nick = user.split('!')[0]
 
-    if (~bot._oper.indexOf(nick)) {
-      op(nick)
-    } 
+    if (~opers.indexOf(nick)) op(nick)
   })
 
   function op(nick) {
-    bot.irc.write('MODE ' + bot.channel + ' +o ' + nick)
+    bot.irc.write('MODE '+ bot.channel +' +o '+ nick)
   }
 }
