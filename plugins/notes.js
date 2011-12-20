37dda2d9cbdac1976e37a5616fc33d86486c7667
Damian Senn <damian.senn@gmail.com>
2011-12-20 11:33:45 +0100

    print notice instead of writing into channel for notes plugin

--- plugins/notes.js
+++ plugins/notes.js
@@ -3,51 +3,52 @@ var Commands = require('../lib/commands')
 Commands.add('leavenote'
   , 'Stores a note for a idleing user, until he returns.'
   , function(bot) {
-
     bot._notes = {}
 
-    /* give note to user on join */
+    // Give note to user on join
     bot.irc.on('join', function(user) {
       printAllNotes(user)
     })
 
-    /* or if user writes to the channel */
-    /* future version could maybe check idle status */
+    // Or if user writes to the channel
+    // future version could maybe check idle status
     bot.irc.on('privmsg', function(user, message) {
       printAllNotes(user)
     })
 
     function printAllNotes(user) {
-      var nick = user.split('!')[0]
-      if (bot._notes[nick] && bot._notes[nick].length > 0) {
-        notes = bot._notes[nick]
-        bot.say('people left notes for you '+ nick +':')
-        for (noteIndex in notes) {
-          note = notes[noteIndex]
-          d = note.time
-          timestamp = d.getDate() +'.'+ (d.getMonth() + 1) + '.'
-                      +' '+ 
-                      d.getHours() +':'+ d.getMinutes();
+      var nick  = user.split('!')[0]
+        , notes = bot._notes[nick]
 
-          bot.say('from '+ note.from +', added '+ timestamp +': '+ note.note)
-        }
+      if (notes && notes.length > 0) {
+        bot.notice(nick, 'people left notes for you:')
+
+        notes.forEach(function(note) {
+          var d          = note.time
+            , timestamp = d.getDate() +'.'+ (d.getMonth() + 1) + '.'
+                          +' '+
+                          d.getHours() +':'+ d.getMinutes()
+
+          bot.notice(nick,
+            'from '+ note.from +', added '+ timestamp +': '+ note.note)
+        })
+
         bot._notes[nick] = []
       }
     }
   }
   , function(from, to, message) {
-
-    var self = this
-      , bot = self._bot
+    var self   = this
+      , bot    = self._bot
       , noteTo = message.split(' ')[0]
-      , note = message.slice(noteTo.length + 1)
+      , note   = message.slice(noteTo.length + 1)
 
     if (note.length == 0) { 
       return say('No note given!')
     }
 
     if (bot._notes[noteTo]) {
-      // already added a note
+      // Already added a note
       bot._notes[noteTo].push({ 'from' : from
                               , 'nick' : noteTo
                               , 'note' : note
@@ -55,14 +56,14 @@ Commands.add('leavenote'
                               })
     }
     else {
-      // first note
+      // First note
       bot._notes[noteTo] = [{ 'from' : from
                             , 'nick' : noteTo
                             , 'note' : note
                             , 'time' : new Date
                             }]
     } 
-    
+
     function say(text) {
       self.say(from, to, from +': '+ text)
     }
