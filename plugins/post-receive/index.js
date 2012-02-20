982f4192f590a6069f6723d2dc70c413550f557d
Damian Senn <damian.senn@gmail.com>
2012-02-20 21:05:29 +0100

    add post-receive hook handler for nib repository :)

---
+++ plugins/post-receive/index.js
@@ -0,0 +1,37 @@
+var http        = require('http')
+  , querystring = require('querystring')
+
+module.exports = function(bot) {
+  var server = http.createServer(function(request, respone) {
+    if (request.headers['x-github-event'] != 'push') return
+
+    var data = ''
+
+    request.on('data', function(chunk) { data += chunk })
+    request.on('end',  function() {
+      var r = JSON.parse(querystring.parse(data).payload)
+
+      bot.say([ r.pusher.name
+              , ' pushed to '
+              , r.repository.name
+              , ':'
+              ].join(''))
+
+      bot.say(r.compare)
+
+      for (var i = 0, l = r.commits.length; i < l; ++i) {
+        var commit = r.commits[i]
+
+        bot.say([ '- '
+                , commit.author.username
+                , ': '
+                , commit.message.replace(/\n+/g, ' ')
+                ].join(''))
+      }
+    })
+  })
+
+  server.listen(31337)
+
+  bot.on('quit', function() { server.close() })
+}
