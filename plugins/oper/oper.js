afc7b840dd9d45aaae6cb55fd8b336a3f4ceb919
Damian Senn <damian.senn@gmail.com>
2012-03-03 13:47:14 +0100

    op all opers in opers.json if nib gets op

--- plugins/oper/oper.js
+++ plugins/oper/oper.js
@@ -10,6 +10,17 @@ module.exports = function(bot) {
     if (~opers.indexOf(nick)) op(nick)
   })
 
+  bot.irc.on('mode', function(from, b) {
+    b = b.split(' ')
+
+    if (b[0] == bot.channel && b[1] == '+o' && b[2] == bot.nick) {
+      bot.irc.names([bot.channel], function(names) {
+        names[bot.channel].filter(function(n) { return ~opers.indexOf(n) })
+                          .forEach(op)
+      })
+    }
+  })
+
   function op(nick) {
     bot.irc.write('MODE '+ bot.channel +' +o '+ nick)
   }
