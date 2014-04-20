ce50854f876506beab8795bc7ff4f31502319744
Damian Senn <damian.senn@gmail.com>
2014-04-20 12:34:10 +0200

    Parse 9gag and imgur pictures for piclog

--- plugins/piclog/index.js
+++ plugins/piclog/index.js
@@ -1,25 +1,55 @@
 var fs      = require('fs')
+  , url     = require('url')
   , path    = require('path')
   , mkdirp  = require('mkdirp')
   , request = require('request')
+  , cheerio = require('cheerio')
+
+var selector = { '9gag.com':   '.badge-item-img'
+               , 'imgur.com':  '#image img'
+               }
 
 function downloadImage(uri, file) {
   request.head(uri, function(err, res, body){
-    if (err) return console.err(err)
+    if (err) return console.error(err)
 
-    if (res.headers['content-type'].startsWith('image/')) {
+    if (res.headers['content-type'].contains('image/')) {
       mkdirp(path.dirname(file), function(err) {
         if (err) return console.error(err)
 
         request(uri).pipe(fs.createWriteStream(file))
       })
     }
+    else if (res.headers['content-type'].contains('text/html')) {
+      var urlObj = url.parse(uri)
+        , sel    = selector[urlObj.host]
+
+      if (sel) request(uri, function(err, res, body) {
+        if (err || res.statusCode != 200) return
+
+        var $   = cheerio.load(body)
+          , $el = $(sel)
+          , uri = $el.attr('src')
+
+        if (uri) {
+          if (uri.startsWith('//')) uri = 'http:'+ uri
+
+          downloadImage(uri, file)
+        }
+      })
+    }
   })
+}
+
+if (!String.prototype.contains) {
+  String.prototype.contains = function(s) {
+    return !!~this.indexOf(s)
+  }
 }
 
 if (!String.prototype.startsWith) {
   String.prototype.startsWith = function(s) {
-    return !this.indexOf(s)
+    return this.indexOf(s) === 0
   }
 }
 
