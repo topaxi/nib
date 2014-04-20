0fea47d668c434e429eefa088248004de582aafd
Damian Senn <damian.senn@gmail.com>
2014-04-20 13:15:32 +0200

    Make channels for 1337 plugin configurable

--- plugins/1337.js
+++ plugins/1337.js
@@ -2,12 +2,12 @@
 /**
  * Simple plugin which writes "13:37" @ 13:37 into the channel
  */
-module.exports = function(bot) {
+module.exports = function(bot, options) {
   var interval = setInterval(function() {
     var d = new Date
 
     if (d.getHours() == 13 && d.getMinutes() == 37) {
-      bot.say('13:37')
+      bot.say(options && options.channels || bot.channels, '13:37')
     }
   }, 1000 * 60)
 
