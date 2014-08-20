c658dc1f804e51504a31ddc83c6633ed5d404788
Damian Senn <damian.senn@gmail.com>
2014-08-20 13:35:36 +0200

    Allow video/ mimetypes in piclog too.

--- plugins/piclog/index.js
+++ plugins/piclog/index.js
@@ -13,7 +13,8 @@ function downloadImage(uri, file) {
   request.head(uri, function(err, res, body){
     if (err) return console.error(err)
 
-    if (res.headers['content-type'].contains('image/')) {
+    if (res.headers['content-type'].contains('image/') ||
+        res.headers['content-type'].contains('video/')) {
       mkdirp(path.dirname(file), function(err) {
         if (err) return console.error(err)
 
