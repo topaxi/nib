a6150f96c951beab505249747d863a2cc6502e2a
Lukas Grossar <lukas.grossar@gmail.com>
2015-07-09 10:43:17 +0200

    add odin trigger to bofh.js

--- plugins/bofh.js
+++ plugins/bofh.js
@@ -13,6 +13,7 @@ module.exports = Command.extend({
                   , 'kaputt'
                   , 'help'
                   , 'parallels'
+                  , 'odin'
                   ]
   , init: function(bot) {
       var self = this
