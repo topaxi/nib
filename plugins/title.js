a13d697ec390c67bca6e405929b06166188af1ef
Lukas Grossar <lukas@grossar.net>
2015-10-24 20:41:05 +0200

    added detection of youtu.be URL to title.js

--- plugins/title.js
+++ plugins/title.js
@@ -51,7 +51,7 @@ module.exports = function(bot, options) {
       })
     }
     
-    var youtube = /https?:\/\/(?:www\.)?youtube.com\/watch\?[^\s]+/.exec(msg)
+    var youtube = /https?:\/\/(?:www\.)?i(?:youtube.com|youtu.be)\/(?:v\/|embed\/|watch(?:\?v=|\/))?[a-zA-Z0-9-]+/.exec(msg)
     if (youtube) {
       return extractYoutubeTitle(youtube[0], function(title) {
         bot.say(channel, 'Youtube: ' + title)
